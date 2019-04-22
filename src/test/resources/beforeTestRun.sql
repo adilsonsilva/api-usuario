ac7158fafa84e8eb30b6a64ff1412238ecdf2832
adilson.ro.silva@gmail.com <adilson.ro.silva@gmail.com>
2019-04-21 23:12:54 -0300

    Melhoria testes unitarios - ajustes 5

--- src/test/resources/beforeTestRun.sql
+++ src/test/resources/beforeTestRun.sql
@@ -1,14 +1,3 @@
-CREATE TABLE usuario (
-	id int4 NOT NULL,
-	ativo bool NULL,
-	data_cadastro timestamp NULL,
-	data_expiracao timestamp NULL,
-	email varchar(50) NULL,
-	nome varchar(50) NULL,
-	senha varchar(10) NULL,
-	CONSTRAINT usuario_pkey PRIMARY KEY (id)
-);
-
 insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (1, 'Adilson Silva', '123456', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00', 'teste@teste.com.br');
 
 insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (2, 'Teste da Silva', '1234aa', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00','teste1@teste.com.br');
