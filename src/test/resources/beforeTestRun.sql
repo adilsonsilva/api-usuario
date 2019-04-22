19846288fcf20688939a08803df7eb7890a80399
adilson.ro.silva@gmail.com <adilson.ro.silva@gmail.com>
2019-04-21 23:17:00 -0300

    Melhoria testes unitarios - ajustes 6

--- src/test/resources/beforeTestRun.sql
+++ src/test/resources/beforeTestRun.sql
@@ -1,5 +1,5 @@
-insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (1, 'Adilson Silva', '123456', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00', 'teste@teste.com.br');
+insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (111, 'Adilson Silva', '123456', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00', 'teste@teste.com.br');
 
-insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (2, 'Teste da Silva', '1234aa', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00','teste1@teste.com.br');
+insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (112, 'Teste da Silva', '1234aa', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00','teste1@teste.com.br');
 
-insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (3, 'Adilson Rodrigues', 'aa3456', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00','teste2@teste.com.br');
+insert into usuario (id, nome, senha, ativo, data_cadastro, data_expiracao, email) values (113, 'Adilson Rodrigues', 'aa3456', 1, '2019-04-20 23:00:00', '2020-04-20 23:00:00','teste2@teste.com.br');
