48713ad0e9d2f7a4a972e709e89423a7a879442e
adilson.ro.silva@gmail.com <adilson.ro.silva@gmail.com>
2019-05-02 21:38:02 -0300

    Remoção da segurança

--- src/main/java/br/com/ars/apiusuario/ApiUsuarioApplication.java
+++ src/main/java/br/com/ars/apiusuario/ApiUsuarioApplication.java
@@ -2,8 +2,9 @@ package br.com.ars.apiusuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApiUsuarioApplication {
 
 	public static void main(String[] args) {
